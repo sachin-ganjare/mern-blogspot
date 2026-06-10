3856c7835d6fba8e9482fdffa047ebfb08dc49ba
sganjare29 <sganjare29@gmail.com>
2026-06-11 00:41:35 +0530

    create and add footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import SignUp from './pages/SignUp'
 import Projects from './pages/Projects'
 import About from './pages/About'
 import Header from './components/Header.jsx'
-
+import Footer from './components/Footer.jsx'
 function App() {
   return (
     <BrowserRouter>
@@ -20,6 +20,7 @@ function App() {
         <Route path="/projects" element={<Projects/>}/>
         <Route path="/about" element={<About/>}/>
       </Routes>
+        <Footer/>
     </BrowserRouter>
   )
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,68 @@
+import { Footer, FooterDivider, FooterLink, FooterLinkGroup, FooterTitle, FooterCopyright, FooterIcon } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+import {BsFacebook, BsGithub, BsInstagram, BsTwitterX} from 'react-icons/bs';
+
+export default function FooterCom() {
+    return (
+        <Footer container className='border border-t-8 border-teal-500'>
+            <div className='w-full max-w-7xl mx-auto'>
+                <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
+                    <div className='mt-5'>
+                        <Link to="/" className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'>
+                            <span className='px-2 py-1 bg-linear-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white' >Sachin's</span>
+                            Blog
+                        </Link>
+                    </div>
+                    <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6 ">
+                        <div>
+                            <FooterTitle title='About' />
+                            <FooterLinkGroup col>
+                                <FooterLink href='https://mytechsuweb.blogspot.com/' target='_blank' rel='noopener noreferrer'>
+                                    MyTechsu's Blogspot
+                                </FooterLink>
+                                <FooterLink href='https://mytechsuweb.blogspot.com/' target='_blank' rel='noopener noreferrer'>
+                                    MyTechsu's Blogspot
+                                </FooterLink>
+                                <FooterLink href='https://mytechsuweb.blogspot.com/' target='_blank' rel='noopener noreferrer'>
+                                    MyTechsu's Blogspot
+                                </FooterLink>
+                            </FooterLinkGroup>
+                        </div>
+                        <div>
+                            <FooterTitle title='Follow Us' />
+                            <FooterLinkGroup col>
+                                <FooterLink href='#' target='_blank' rel='noopener noreferrer'>
+                                    GitHub
+                                </FooterLink>
+                                <FooterLink href='#' target='_blank' rel='noopener noreferrer'>
+                                    Facebook
+                                </FooterLink>
+                            </FooterLinkGroup>
+                        </div>
+                        <div>
+                            <FooterTitle title='Legal' />
+                            <FooterLinkGroup col>
+                                <FooterLink href='https://mytechsuweb.blogspot.com/' target='_blank' rel='noopener noreferrer'>
+                                    Privacy Policy
+                                </FooterLink>
+                                <FooterLink href='https://mytechsuweb.blogspot.com/' target='_blank' rel='noopener noreferrer'>
+                                    Terms &amp; Conditions
+                                </FooterLink>
+                            </FooterLinkGroup>
+                        </div>
+                    </div>
+                </div>
+                <FooterDivider/>
+                <div className="w-full sm:flex sm:items-center sm:justify-between">
+                    <FooterCopyright href="#" by="Sachin's Blog" year={new Date().getFullYear()}/>
+                    <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+                        <FooterIcon href='#' icon={BsFacebook}/>
+                        <FooterIcon href='#' icon={BsInstagram}/>
+                        <FooterIcon href='#' icon={BsTwitterX}/>
+                        <FooterIcon href='#' icon={BsGithub}/>
+                    </div>
+                </div>
+            </div>
+        </Footer>
+    )
+}
